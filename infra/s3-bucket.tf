28cfebcd35f675708bcd5bd450759b0998ad3778
hiranthaPeiris <Hirantha.Peiris@sysco.com>
2024-05-27 10:57:01 +0530

    workflow mods -2

--- infra/s3-bucket.tf
+++ infra/s3-bucket.tf
@@ -1,8 +1,8 @@
 # Create an S3 bucket
 resource "aws_s3_bucket" "my_bucket" {
-  bucket = "my-unique-bucket-name-12345"  # Ensure this bucket name is unique across all of AWS
+  bucket = "my-unique-bucket-name-12345" # Ensure this bucket name is unique across all of AWS
 
-  
+
   tags = {
     Name        = "MyBucket"
     Environment = "Dev"
